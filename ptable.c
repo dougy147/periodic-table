bd81b77c4f48d80a1c3ef879091914ad8f93528f
dougy147 <16362303+dougy147@users.noreply.github.com>
2024-06-12 17:41:50 +0200

    Show more properties in single mode view.

--- ptable.c
+++ ptable.c
@@ -3,10 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define DEBUG  0
 #define WIDTH  1600
 #define HEIGHT 900
-#define FONT_SIZE WIDTH/32
+#define FS WIDTH/32 // FONT SIZE
 #define NB_ELEMENTS 118
 
 #define NB_CELL_WIDTH  19
@@ -27,6 +26,16 @@ typedef struct element {
     char color[10];
     int x_position_in_table;
     int y_position_in_table;
+
+    /* Stored as strings because of unknown values in CSV file */
+    char melting_point[100];
+    char boiling_point[100];
+    char density[100];
+    char phase_at_room_temp[100];
+    char classification[100];
+    char period_number[100];
+    char group_number[100];
+    char group_name[100];
 } Element;
 
 /* Box containing elements */
@@ -35,48 +44,66 @@ typedef struct box {
     Rectangle rectangle;
 } Box;
 
-void display_single_element(Element element, Font font) {
-    int ho = HEIGHT / 8; // height offset
+Color string_to_color(char color_string[]) {
+    Color color;
+    if      (strcmp("RED",    color_string) == 0) { color = RED; }
+    else if (strcmp("YELLOW", color_string) == 0) { color = YELLOW; }
+    else if (strcmp("BLUE",   color_string) == 0) { color = BLUE; }
+    else if (strcmp("GREEN",  color_string) == 0) { color = GREEN; }
+    return color;
+}
 
-    /* Display name */
-    // DrawTextEx(fonts[i], messages[i], positions[i], fonts[i].baseSize*2.0f, (float)spacings[i], colors[i]);
-    DrawTextEx(font, TextFormat("%s", element.name),(Vector2){WIDTH/2 - (strlen(element.name) * FONT_SIZE / 2.0) / 2.0,FONT_SIZE},FONT_SIZE,0,YELLOW);
+void display_single_element(Element e, Font font) {
+    int wo = WIDTH / 8;
+    int ho = HEIGHT / 8;
 
     /* Surrounding square */
-    int square_side = HEIGHT - 2 * ho;
-    int wo = (WIDTH - square_side) / 2;
-    int border_size = FONT_SIZE / 4;
-    DrawRectangleLinesEx((Rectangle){wo, ho, square_side, square_side}, border_size, YELLOW);
+    int sqs = HEIGHT - 4 * ho; // square side size
+    int bs  = FS / 4;          // border size
+
+    /* Display name */
+    DrawTextEx(font,TextFormat("%s", e.name),(Vector2){wo+(sqs/2)-(strlen(e.name)*FS/2.0)/2.0,ho+FS},FS,0,string_to_color(e.color));
+
+    /* Draw square */
+    DrawRectangleLinesEx((Rectangle){wo,ho+ho,sqs,sqs},bs,string_to_color(e.color));
 
     /* Display atomic number, symbol, atomic mass inside of square */
     char atomic_number_string[3];
-    sprintf(atomic_number_string,"%d",element.atomic_number);
-    DrawTextEx(font,TextFormat("%d", element.atomic_number),
-	    (Vector2){WIDTH/2 - (strlen(atomic_number_string) * FONT_SIZE / 2.0) / 2, ho + FONT_SIZE},
-	    FONT_SIZE,
-	    0,
-	    BLUE);
+    sprintf(atomic_number_string,"%d",e.atomic_number);
+    DrawTextEx(
+	    font,
+	    TextFormat("%d", e.atomic_number),
+	    (Vector2){wo+(sqs/2)-(strlen(atomic_number_string)*FS/2.0)/2, ho+ho+FS},
+	    FS,0,string_to_color(e.color));
 
     int symbol_font_size_factor = 4;
-    DrawTextEx(font,TextFormat("%s", element.symbol),
-	    (Vector2){WIDTH/2 - (strlen(element.symbol) * (FONT_SIZE * symbol_font_size_factor) / 2.0) / 2, ho + square_side / 2 - (symbol_font_size_factor * FONT_SIZE) / 2},
-	    FONT_SIZE * symbol_font_size_factor,
-	    0,
-	    YELLOW);
+    DrawTextEx(font,TextFormat("%s", e.symbol),
+	    (Vector2){wo+(sqs/2)-(strlen(e.symbol)*(FS*symbol_font_size_factor)/2.0)/2,ho+ho+sqs/2-(symbol_font_size_factor*FS)/2},
+	    FS*symbol_font_size_factor,0,string_to_color(e.color));
 
     char mass_string[15];
-    sprintf(mass_string,"%.6f",element.atomic_mass);
+    sprintf(mass_string,"%.6f",e.atomic_mass);
     DrawTextEx(font,
 	    mass_string,
-	    (Vector2){WIDTH/2 - (strlen(mass_string) * FONT_SIZE / 2.0) / 2, ho + square_side - 2 * FONT_SIZE},
-	    FONT_SIZE,
-	    0,
-	    BLUE);
+	    (Vector2){wo+(sqs/2)-(strlen(mass_string)*FS/2)/2,ho+ho+sqs-2*FS},
+	    FS,0,string_to_color(e.color));
+
+    /* Display characteristics list (should avoid repetition with for loop. store characteristics in array?) */
+    DrawTextEx(font,TextFormat("Atomic number: %d", e.atomic_number), (Vector2){wo+sqs+wo/2,ho+0*FS*1.2},FS,0,DARKGRAY);
+    DrawTextEx(font,TextFormat("Atomic mass: %f", e.atomic_mass), (Vector2){wo+sqs+wo/2,ho+1*FS*1.2},FS,0,DARKGRAY);
+    DrawTextEx(font,TextFormat("Melting point (K): %s", e.melting_point), (Vector2){wo+sqs+wo/2,ho+2*FS*1.2},FS,0,DARKGRAY);
+    DrawTextEx(font,TextFormat("Boiling point (K): %s", e.boiling_point), (Vector2){wo+sqs+wo/2,ho+3*FS*1.2},FS,0,DARKGRAY);
+    DrawTextEx(font,TextFormat("Density (g/cm3): %s", e.density), (Vector2){wo+sqs+wo/2,ho+4*FS*1.2},FS,0,DARKGRAY);
+    DrawTextEx(font,TextFormat("Phase (room temp): %s", e.phase_at_room_temp), (Vector2){wo+sqs+wo/2,ho+5*FS*1.2},FS,0,DARKGRAY);
+    DrawTextEx(font,TextFormat("Element classification: %s", e.classification), (Vector2){wo+sqs+wo/2,ho+6*FS*1.2},FS,0,DARKGRAY);
+    DrawTextEx(font,TextFormat("Period number: %s", e.period_number), (Vector2){wo+sqs+wo/2,ho+7*FS*1.2},FS,0,DARKGRAY);
+    DrawTextEx(font,TextFormat("Group number: %s", e.group_number), (Vector2){wo+sqs+wo/2,ho+8*FS*1.2},FS,0,DARKGRAY);
+    DrawTextEx(font,TextFormat("Group name: %s", e.group_name), (Vector2){wo+sqs+wo/2,ho+9*FS*1.2},FS,0,DARKGRAY);
 }
 
 void display_full_table(Element *elements, Font font) {
     int wo = WIDTH  / 16;  /* width offset */
-    int ho = HEIGHT / 16; /* height offset */
+    int ho = HEIGHT / 16;  /* height offset */
     int w_table = (WIDTH  - 2.0 * wo);
     int h_table = (HEIGHT - 2.0 * ho);
     int w_cell = w_table / NB_CELL_WIDTH; // cell width
@@ -97,49 +124,37 @@ void display_full_table(Element *elements, Font font) {
     //}
 
     /* Place elements inside their respective box */
-    Color color;
     for (int i = 0; i < NB_ELEMENTS; i++) {
-	if      (strcmp("RED",    elements[i].color) == 0) { color = RED; }
-	else if (strcmp("YELLOW", elements[i].color) == 0) { color = YELLOW; }
-	else if (strcmp("BLUE",   elements[i].color) == 0) { color = BLUE; }
-	else if (strcmp("GREEN",  elements[i].color) == 0) { color = GREEN; }
+	Color color = string_to_color(elements[i].color);
 	int box_size_x = w_cell / 1.2;
 	int box_size_y = h_cell / 1.2;
         DrawRectangle(
         	wo + (w_cell / 2 - box_size_x / 2) + elements[i].x_position_in_table * w_cell,
         	ho + (h_cell / 2 - box_size_y / 2) + elements[i].y_position_in_table * h_cell,
-        	box_size_x,
-        	box_size_y,
-        	color
-        	);
+        	box_size_x,box_size_y,color);
 	DrawText(
 		TextFormat("%d", elements[i].atomic_number),
-        	wo + (w_cell / 2 - FONT_SIZE/2) + elements[i].x_position_in_table * w_cell,
+        	wo + (w_cell / 2 - FS/2) + elements[i].x_position_in_table * w_cell,
         	ho + (0.1 * h_cell) + elements[i].y_position_in_table * h_cell,
-		w_cell/3.5,
-		DARKGRAY
-		);
+		w_cell/3.5,DARKGRAY);
 	DrawTextEx(
 		font,
 		TextFormat("%s", elements[i].symbol),
-        	(Vector2){wo + (w_cell / 2 - (strlen(elements[i].symbol) * FONT_SIZE / 3) / 2) + elements[i].x_position_in_table * w_cell, ho + (h_cell / 2 - (FONT_SIZE / 3) / 2) + elements[i].y_position_in_table * h_cell},
-		FONT_SIZE/1.5,
-		0,
-		BLACK
-		);
+        	(Vector2){wo + (w_cell / 2 - (strlen(elements[i].symbol) * FS / 3) / 2) + elements[i].x_position_in_table * w_cell, ho + (h_cell / 2 - (FS / 3) / 2) + elements[i].y_position_in_table * h_cell},
+		FS/1.5,0,BLACK);
     }
 }
 
 void parse_periodic_table_csv(char *filename, Element *elements, Box *boxes) {
     FILE *csv_file = fopen(filename, "r");
     bool csv_has_headers = true;
-    int buffer_size = 20000;
+    int  buffer_size = 20000;
     char buffer[buffer_size];
-    int current_atom = 0;
-    int characteristics_index = 0;
+    int  current_atom = 0;
+    int  characteristics_index = 0;
     Element element;
     Box box;
-    /* AtomicNumber,Element,Symbol,AtomicMass,TableRow,TableCol,BoxColor, */
+    /* AtomicNumber,Element,Symbol,AtomicMass,TableRow,TableCol,BoxColor,...*/
     for(;;) {
 	int res = fread(buffer, 1, buffer_size, csv_file); // read the file in one go
 	if (ferror(csv_file)) {
@@ -155,16 +170,6 @@ void parse_periodic_table_csv(char *filename, Element *elements, Box *boxes) {
 
 	    if (buffer[i] == '\n') {
 		elements[current_atom] = element;
-#if 1
-		printf("-------\n");
-		printf("Imported \"%s\"\n", element.name);
-		printf("    Atomic number: %d\n", element.atomic_number);
-		printf("    Symbol: %s\n", element.symbol);
-		printf("    Atomic mass: %f\n", element.atomic_mass);
-		printf("    Table position: x=%d, y=%d\n", element.x_position_in_table,element.y_position_in_table);
-		printf("    Box color: %s\n", element.color);
-#endif
-
 		Rectangle box_rectangle;
 		int wo = WIDTH  / 16;  /* width offset */
     		int ho = HEIGHT / 16; /* height offset */
@@ -191,11 +196,7 @@ void parse_periodic_table_csv(char *filename, Element *elements, Box *boxes) {
 		Element element;
 	    }
 
-	    if (characteristics_index > 6) {
-	        continue;
-	    }
-
-	    char current_characteristic[15];
+	    char current_characteristic[150];
 	    int  index = 0; // pretty inefficient way i guess
 	    while (buffer[i] != ',' && buffer[i] != '\n') {
 		current_characteristic[index] = buffer[i];
@@ -226,8 +227,31 @@ void parse_periodic_table_csv(char *filename, Element *elements, Box *boxes) {
 		case 6:
 		    strcpy(element.color,current_characteristic);
 		    break;
+		case 7:
+		    strcpy(element.melting_point,current_characteristic);
+		    break;
+		case 8:
+		    strcpy(element.boiling_point,current_characteristic);
+		    break;
+		case 9:
+		    strcpy(element.density,current_characteristic);
+		    break;
+		case 10:
+		    strcpy(element.phase_at_room_temp,current_characteristic);
+		    break;
+		case 11:
+		    strcpy(element.classification,current_characteristic);
+		    break;
+		case 12:
+		    strcpy(element.period_number,current_characteristic);
+		    break;
+		case 13:
+		    strcpy(element.group_number,current_characteristic);
+		    break;
+		case 14:
+		    strcpy(element.group_name,current_characteristic);
+		    break;
 	    }
-
 	    if (buffer[i] == ',') characteristics_index++;
 	}
 
@@ -254,9 +278,7 @@ int main(void) {
     Element elements[NB_ELEMENTS];
     Box boxes[NB_ELEMENTS]; // each element resides in its own box
 
-    //char filename[] = "Periodic Table of Elements.csv";
     char filename[] = "elements.csv";
-
     parse_periodic_table_csv(filename, elements, boxes);
 
     int view_mode = TABLE_VIEW;
@@ -267,7 +289,7 @@ int main(void) {
     SetConfigFlags(FLAG_MSAA_4X_HINT);
     SetTargetFPS(30);
 
-    Font font = LoadFontEx("fonts/LinBiolinum_Rah.ttf",FONT_SIZE*5,NULL,0);
+    Font font = LoadFontEx("fonts/LinBiolinum_Rah.ttf",FS*5,NULL,0);
 
     while (!WindowShouldClose()) {
 	BeginDrawing();
@@ -276,8 +298,8 @@ int main(void) {
 	if (view_mode == TABLE_VIEW) {
 	    DrawText(
 		    "Periodic table",
-		    WIDTH/2 - (strlen("periodic table") * FONT_SIZE / 2) / 2, FONT_SIZE,
-		    FONT_SIZE,
+		    WIDTH/2 - (strlen("periodic table") * FS / 2) / 2, FS,
+		    FS,
 		    DARKGRAY);
 	    display_full_table(elements, font);
 	} else if (view_mode == SINGLE_VIEW) {
@@ -286,14 +308,8 @@ int main(void) {
 
 	hovered_element = mouse_hovers_element(boxes, elements);
 	if (hovered_element >= 0 && view_mode == TABLE_VIEW) {
-	    DrawRectangleLinesEx(
-		    boxes[hovered_element-1].rectangle,
-		    5,
-		    BLACK);
-	    DrawRectangleLinesEx(
-		    boxes[hovered_element-1].rectangle,
-		    2,
-		    LIGHTGRAY);
+	    DrawRectangleLinesEx(boxes[hovered_element-1].rectangle,5,BLACK);
+	    DrawRectangleLinesEx(boxes[hovered_element-1].rectangle,2,LIGHTGRAY);
 	    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && view_mode == TABLE_VIEW) {
 		view_mode = SINGLE_VIEW;
 		element_index = hovered_element - 1;
@@ -303,14 +319,14 @@ int main(void) {
 	/* In single view, rotate through elements when clicking. */
 	if (view_mode == SINGLE_VIEW && (IsKeyPressed(KEY_D) || GetMouseWheelMove() > 0)) {
 	    element_index++;
-	    if (element_index >= NB_ELEMENTS - 1) element_index = 0;
+	    if (element_index >= NB_ELEMENTS) element_index = 0;
 	}
 	if (view_mode == SINGLE_VIEW && (IsKeyPressed(KEY_A) || GetMouseWheelMove() < 0)) {
 	    element_index--;
 	    if (element_index < 0) element_index = NB_ELEMENTS - 1;
 	}
 	/* To go back to full table */
-	if (view_mode == SINGLE_VIEW && IsKeyPressed(KEY_S)) {
+	if (view_mode == SINGLE_VIEW && (IsKeyPressed(KEY_S) || IsMouseButtonPressed(MOUSE_RIGHT_BUTTON))) {
 	    view_mode = TABLE_VIEW;
 	}
 
